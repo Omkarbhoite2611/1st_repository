769b1e07648cc759597c803f3fa6d9005ef3d0fe
Omkarbhoite2611 <omkarbhoite2022@gmail.com>
2025-09-18 07:30:50 +0530

    Added program to find the greatest of three numbers in branch2

---
+++ prog1.py
@@ -0,0 +1,16 @@
+
+# This is 1st program in 2nd branch
+
+# Program to find the greater of three numbers
+
+
+num1 = int(input("Enter first number : "))
+num2 = int(input("Enter second number : "))
+num3 = int(input("Enter third number : "))
+
+if (num1 > num2) and (num1 > num3):
+    print(num1 ," is the greatest number")
+elif (num2 > num1) and (num2 > num3):
+    print(num2 , " is the greatest number")
+else:
+    print(num3 , " is the greatest number")
