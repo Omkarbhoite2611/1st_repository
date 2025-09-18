89e3b221a6ca95b072ac60d1c0ad6c376cd14491
Omkarbhoite2611 <omkarbhoite2022@gmail.com>
2025-09-18 08:41:21 +0530

    Added factorial program in branch4

---
+++ factorial.py
@@ -0,0 +1,15 @@
+
+
+# Program to create factorial of a number
+
+num = int(input("Enter a number : "))
+factorial = 1
+
+if num < 0:
+    print("Factorial does not exist not negative numbers !")
+elif num == 0:
+    print("Factorial of 0 is 1")
+else:
+    for i in range(1 , num + 1):
+        factorial *= i
+    print(f"Factorial of {num} is {factorial} !")
